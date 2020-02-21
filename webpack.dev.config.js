a0e020c6d4de1f7ab69f3c7451f2f9f2f5445481
apontes <nelopontes+assignments@gmail.com>
2020-02-21 15:55:00 +0000

    improve development config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,11 @@
-const merge = require('webpack-merge');
+const merge = require("webpack-merge");
+const path = require("path");
 const commonConfig = require("./webpack.common.config");
 
-module.exports =  merge(commonConfig, {
-  mode: "development"
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  }
 });
